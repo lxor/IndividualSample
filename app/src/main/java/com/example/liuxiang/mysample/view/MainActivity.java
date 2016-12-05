c77df79d93f7d0848cb7a9d51ebc7bbb93ff338c
lxor <liuxiang90627@yeah.net>
2016-12-05 18:06:39 +0800

    //...

--- app/src/main/java/com/example/liuxiang/mysample/view/MainActivity.java
+++ app/src/main/java/com/example/liuxiang/mysample/view/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.liuxiang.mysample;
+package com.example.liuxiang.mysample.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.liuxiang.mysample.R;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/liuxiang/mysample/view/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.liuxiang.mysample.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.liuxiang.mysample.R;
+
+/**
+ * Created by liuxiang
+ * email: liuxiang90627@yeah.net
+ */
+
+
+public class SplashActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
